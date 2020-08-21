1f24b188e1e34e8ef5ff7108bc42b1d68b5d1386
Michael <michaeljanschiumo@gmail.com>
2020-08-21 17:09:16 -0400

    checking

--- server.js
+++ server.js
@@ -13,3 +13,5 @@ app.use('/api/movies', require('./routes/movies'));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+//is this working
