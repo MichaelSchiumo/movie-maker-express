02181206c4854069d6e9b4dfe61930c1b49fdb48
Michael <michaeljanschiumo@gmail.com>
2020-08-21 19:58:01 -0400

    all routes defined

---
+++ models/Movie.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const MovieSchema = mongoose.Schema({
+  //this is how we verify for user id when creating movies lesson47
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  img_url: {
+    type: String,
+    required: true,
+  },
+  desc: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('movie', MovieSchema);
--- routes/movies.js
+++ routes/movies.js
@@ -1,18 +1,85 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../middleware/auth');
+const User = require('../models/User');
+const Movie = require('../models/Movie');
 
+//Want this to be a public route so all users can see all movies
+//add admin funcitonality
+//going to do it traversy's way first with auth
+//need to remove 'auth' param && middleware
 // @route GET api/movies
 // @desc Get all user's movies
 // @access Private
-router.get('/', (req, res) => {
-  res.send('Get all movies');
+router.get('/', auth, async (req, res) => {
+  try {
+    const movies = await Movie.find({ user: req.user.id }).sort({ date: -1 });
+    res.json(movies);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 //@route POST api/movies
 //@desc Add new movie
 //@access Private
-router.post('/', (req, res) => {
-  res.send('Add Movie');
-});
+//this is where you can require admin
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('title', 'Title is required').not().isEmpty(),
+      check('desc', 'Description is required').not().isEmpty(),
+      check('img_url', 'Img required').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, desc, img_url } = req.body;
+
+    try {
+      const newMovie = new Movie({
+        title,
+        desc,
+        img_url,
+        user: req.user.id,
+      });
+
+      const movie = await newMovie.save();
+
+      res.json(movie);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+//Delete a Movie (as an admin)
+// router.delete(':/id', auth, async (req, res) => {
+//   try {
+//     let movie = await Movie.findById(req.params.id);
+
+//     if (!movie) return res.status(404).json({ msg: 'Movie not found' });
+
+//     //make sure user is an admin
+//     if (movie.user.toString() !== req.user.id) {
+//       return res.status(401).json({ msg: 'Not Authorized' });
+//     }
+
+//     await Movie.findByIdAndRemove(req.params.id);
+//     res.json({ msg: 'Movie was successfully deleted' });
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send('Server Error');
+//   }
+// });
 
 module.exports = router;
