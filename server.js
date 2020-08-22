b5742e209b1406ae5126f396bfb08ea40cb0d26c
Michael <michaeljanschiumo@gmail.com>
2020-08-21 20:56:13 -0400

    added cors to server js

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 const app = express();
 
 //connect DB
 connectDB();
+
+//use cors
+app.use(cors());
 
 //initialize middleware
 app.use(express.json({ extended: false }));
