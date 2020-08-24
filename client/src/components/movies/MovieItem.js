e0df25ade64dec82c7fae233824d8d93cc688b4e
Michael <michaeljanschiumo@gmail.com>
2020-08-24 11:43:59 -0400

    added stars and see reviews button

--- client/src/components/movies/MovieItem.js
+++ client/src/components/movies/MovieItem.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types';
 const MovieItem = ({ movie }) => {
   const { img_url, title, desc } = movie;
 
+  const randStar = Math.floor(Math.random() * 5) + 1;
+
   return (
     <div className='card bg-light align-items:center'>
       <img src={img_url} alt='poster' />
@@ -17,7 +19,10 @@ const MovieItem = ({ movie }) => {
         <span class='fa fa-star'></span>
         <span class='fa fa-star'></span>
       </div>
-      <p className='text-center text-medium'>{desc}</p>
+      <p className='text-center text-medium'>{desc}</p>{' '}
+      <div className='text-center'>
+        <button className='btn btn-primary btn-block'>See Reviews</button>
+      </div>
     </div>
   );
 };
